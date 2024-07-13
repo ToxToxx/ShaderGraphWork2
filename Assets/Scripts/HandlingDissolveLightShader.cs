9a695e3b15491934d324ffef5e857fd05caaee78
Matvey <sinema3d00@gmail.com>
2024-07-13 20:51:00 +0300

    made dissolve shader with direction

--- Assets/Scripts/HandlingDissolveLightShader.cs
+++ Assets/Scripts/HandlingDissolveLightShader.cs
@@ -2,17 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(MeshRenderer))]
 public class HandlingDissolveLightShader : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private const float MAX_TRESHOLD = 1;
+    private const string TRESHOLD_KEY = "_Treshold"; //находится в окне шейдер графа
+    private MeshRenderer _meshRenderer;
+
+    private Coroutine _show;
+
+    private void Awake()
+    => _meshRenderer = GetComponent<MeshRenderer>();
+
+    private void Update()
     {
-        
+        if (Input.GetKeyUp(KeyCode.F))
+        {
+            if(_show != null)
+                StopCoroutine(_show);
+
+            _show = StartCoroutine(Show());
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator Show()
     {
-        
+        float treshold = 0;
+
+        while (treshold < MAX_TRESHOLD)
+        {
+            treshold += Time.deltaTime;
+            _meshRenderer.material.SetFloat(TRESHOLD_KEY, treshold);
+            yield return null;
+        }
     }
 }
